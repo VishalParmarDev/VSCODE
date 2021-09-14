38d379b644265bd2582dcb69d01722626bd58c06
Vishal_Parmar <90678999+VishalParmarDev@users.noreply.github.com>
2021-09-14 13:14:44 +0530

    Add files via upload

---
+++ variables.dart
@@ -0,0 +1,8 @@
+main() {
+  var devName = "Vishal Parmar";
+  var Age = 21;
+  var Height = 5.4;
+  print(
+      "Hello! Friends my name is $devName.I'm $Age year old  and my height is $Height.");
+  
+}
